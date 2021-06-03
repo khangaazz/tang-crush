276ac9bb77a15afedbb73566dd7af7b89b75c7df
khangaazz <85285258+khangaazz@users.noreply.github.com>
2021-06-03 18:07:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babeeee à!',
+    introTitle: 'Trúc Linh ơiiiiiiii!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
